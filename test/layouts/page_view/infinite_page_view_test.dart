b651a9d4ff3410b0807f919622f0bf34b2b035b6
FadyFayezYounan <fadyfayez067@gmail.com>
2024-04-26 23:29:27 +0300

    add more test for page view

--- test/layouts/page_view/infinite_page_view_test.dart
+++ test/layouts/page_view/infinite_page_view_test.dart
@@ -2,13 +2,14 @@ import 'package:easy_infinite_pagination/easy_infinite_pagination.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+const _itemCount = 10;
 void main() {
   group('InfinitePageView - ', () {
     testWidgets('renders correctly', (tester) async {
       // Arrange
       final delegate = PaginationDelegate(
         itemBuilder: (context, index) => Text('Item $index'),
-        itemCount: 10,
+        itemCount: _itemCount,
         onFetchData: () {},
       );
       // Act
@@ -19,13 +20,41 @@ void main() {
       expect(find.byType(InfinitePageView), findsOneWidget);
     });
   });
+  testWidgets('renders the correct number of children', (tester) async {
+    const lastItemKey = ValueKey(9);
+    var childrenCount = 0;
+    // Arrange
+    final delegate = PaginationDelegate(
+      itemBuilder: (context, index) {
+        childrenCount++;
+        return Container(
+          key: ValueKey(index),
+        );
+      },
+      itemCount: _itemCount,
+      onFetchData: () {},
+    );
+    // Act
+    await tester.pumpApp(
+      InfinitePageView(delegate: delegate),
+    );
+    await tester.scrollUntilVisible(
+      find.byKey(lastItemKey),
+      250,
+    );
+
+    // Assert
+    expect(childrenCount, equals(_itemCount));
+    expect(find.byKey(lastItemKey), findsOneWidget);
+  });
+
   testWidgets(
     'calls onFetchData in the initState',
     (tester) async {
       // Arrange
       var onFetchDataCalledCount = 0;
       final delegate = PaginationDelegate(
-        itemCount: 10,
+        itemCount: _itemCount,
         invisibleItemsThreshold: 1,
         itemBuilder: (context, index) => Text(
           'Item $index',
